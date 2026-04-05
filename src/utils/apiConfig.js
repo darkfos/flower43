b9e0d47b9a9cc30e71374b98b36309e05c2bbfbc
darkfos82 <darkfos82@gmail.com>
2026-04-05 22:09:07 +0300

    fix: вернул старый юрл

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://flowers-site161.ru//api',
     timeout: 2800,
     headers: {
         Authorization: `Bearer ${localStorage.getItem('authToken')}`
