419b87380fb326c5f66d2b40b476e4d6a4b68abc
darkfos82 <darkfos82@gmail.com>
2026-04-05 22:04:21 +0300

    fix: пофиксил баг с датой

--- src/pages/ProductDetailPage/index.jsx
+++ src/pages/ProductDetailPage/index.jsx
@@ -33,6 +33,9 @@ const ProductDetailPage = memo(() => {
                 type
             }
         } = detailProductData;
+
+        console.log(detailProductData);
+        
         
         addToCart({
             id,
